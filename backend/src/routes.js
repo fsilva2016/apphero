f20393044e4738216cafdc8f35d9c75b9d60fcb2
Your Name <you@example.com>
2020-04-06 12:32:18 -0300

    app

---
+++ backend/src/routes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const routes = express.Router();
+
+const OngController = require('./controllers/OngControllers');
+const IncidentsController = require('./controllers/IncidentsController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
+
+
+routes.get('/profile', ProfileController.index);
+
+routes.post('/sessions', SessionController.create);
+routes.get('/ongs', OngController.index);
+routes.post('/ongs/', OngController.create);
+
+routes.get('/incidents', IncidentsController.index);
+routes.post('/incidents', IncidentsController.create);
+routes.delete('/incidents/:id', IncidentsController.delete);
+
+
+module.exports = routes;
+
